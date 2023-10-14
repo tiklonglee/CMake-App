c7854a03408c282ad52f751a84325e10a931f523
Tik Long Lee <leetiklong@gmail.com>
2023-10-14 17:46:18 +0800

    [tests] Sample unittest using googletest

--- libraries/core/src/core.cpp
+++ libraries/core/src/core.cpp
@@ -1,5 +1,6 @@
 #include "core/core.hpp"
 
+#include <cstdint>
 #include <iostream>
 
 #include "core_local.hpp"
@@ -10,6 +11,11 @@ namespace core
 auto helloworld() -> void
 {
     std::cout << "Hello World " << current_time() << '\n';
+}
+
+auto factorial(uint32_t number) noexcept -> uint32_t  // NOLINT(misc-no-recursion)
+{
+    return number <= 1 ? 1 : factorial(number - 1) * number;
 }
 
 }  // namespace core
