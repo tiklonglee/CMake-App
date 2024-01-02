fe40d18bbff22c3c7a3d196da61b066d608488fe
Tik Long Lee <leetiklong@gmail.com>
2024-01-02 20:19:31 +0800

    [cmake] Can enable code coverage

---
+++ cmake/Coverage.cmake
@@ -0,0 +1,10 @@
+# Set up code coverage flags
+function(enable_coverage target)
+    if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        message(STATUS "Enabled coverage")
+        target_compile_options(${target} INTERFACE --coverage -O0 -g)
+        target_link_libraries(${target} INTERFACE --coverage)
+    else()
+        message(WARNING "Cannot enable coverage on non-clang/g++ compiler")
+    endif()
+endfunction()
