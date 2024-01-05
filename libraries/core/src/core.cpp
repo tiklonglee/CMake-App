5060c670e7bcd4f07b1e5dcaac034eac7274c0ea
Tik Long Lee <leetiklong@gmail.com>
2024-01-05 15:19:41 +0800

    [configure_files] Sample configure_file to include version info

--- libraries/core/src/core.cpp
+++ libraries/core/src/core.cpp
@@ -2,15 +2,18 @@
 
 #include <cstdint>
 #include <iostream>
+#include <string_view>
 
 #include "core_local.hpp"
+#include "configure_files/version.hpp"
 
 namespace core
 {
 
 auto helloworld() -> void
 {
-    std::cout << "Hello World " << current_time() << '\n';
+    std::cout << "Hello World " << current_time() << '\n'
+              << "Git sha: " << cfg::git_sha << " timestamp: " << cfg::timestamp << '\n';
 }
 
 auto factorial(uint32_t number) noexcept -> uint32_t  // NOLINT(misc-no-recursion)
